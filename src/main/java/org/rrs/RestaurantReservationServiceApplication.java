13e1b1a13dc594d8fc509da2035d8c1ea43f62c3
Ho <Ho@DESKTOP-60OCNAC>
2022-06-17 17:40:59 +0900

    SpringBoot 초기세팅 Git Upload

---
+++ src/main/java/org/rrs/RestaurantReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.rrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantReservationServiceApplication.class, args);
+	}
+
+}
