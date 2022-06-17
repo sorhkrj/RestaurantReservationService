955dce435cdd4076339ed0c22f4e4f4dbe5bda0e
Ho <Ho@DESKTOP-60OCNAC>
2022-06-17 17:54:17 +0900

    .gitignore 설정 후 Upload

---
+++ src/test/java/org/rrs/RestaurantReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
